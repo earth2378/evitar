1b982a5889d414be265acae24867af042d2f26b5
EarthSiwapol <siwapol@blockfint.com>
2020-09-04 21:39:07 +0700

    comment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import src.initNode as init
 
 w3 = Web3(HTTPProvider('http://localhost:8544'))
 
-init.deployContract(w3)
+# init.deployContract(w3)
 # init.getAvailableContract(w3)
 
 # init.checkSuccessfulContract()
