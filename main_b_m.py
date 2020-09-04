05982576f9a36e0f9cade2a136e6782dba0cec04
EarthSiwapol <siwapol@blockfint.com>
2020-09-04 21:39:01 +0700

    support to run all file

--- main_b_m.py
+++ main_b_m.py
@@ -9,8 +9,15 @@ from web3 import Web3, HTTPProvider, IPCProvider, WebsocketProvider
 if __name__ == "__main__":
     w3 = Web3(HTTPProvider('http://localhost:8545'))
     w3_2 = Web3(HTTPProvider('http://localhost:8544'))
-    file = "./evitar_tx_by_hash/0xe.csv"
-    rTx.replayBaseLineAndMaxGas(w3,w3_2,file)
+
+    files = os.listdir("./evitar_tx_by_hash")
+    files.sort()
+    # print(files)
+
+    for file in files:
+        fileName = "./evitar_tx_by_hash/"+file
+        rTx.replayBaseLineAndMaxGas(w3,w3_2,fileName)
+        rTx.backupBM(file)
 
     txF.writeTx(w3,'./result/result_baseline.csv',28)
     txF.writeTx(w3,'./result/result_maxgas.csv',28)
