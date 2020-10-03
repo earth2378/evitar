9760433614d5f336733dce87bb51c191a29e6db8
EarthSiwapol <siwapol@blockfint.com>
2020-10-03 07:46:47 +0700

    make main run only 1 file

--- main_evitar.py
+++ main_evitar.py
@@ -1,35 +1,35 @@
-import math,csv,time,_thread,os,random, sys
-import src.txFunction as txF
+import _thread
+import csv
+import math
+import os
+import random
+import sys
+import time
+
+from web3 import HTTPProvider, IPCProvider, Web3, WebsocketProvider
+
 import src.initNode as init
 import src.replayTx as rTx
-from web3 import Web3, HTTPProvider, IPCProvider, WebsocketProvider
+import src.txFunction as txF
 
 # MAIN!!
-
-
 if __name__ == "__main__":
 
     w3 = Web3(HTTPProvider('http://localhost:8545'))
-    thresh = float(sys.argv[1])
-    wnd = float(sys.argv[2])
+    file = sys.argv[1]
+    thresh = float(sys.argv[2])
+    wnd = int(sys.argv[3])
     directory = "./evitar_tx_by_hash/"
 
-    files = os.listdir("./evitar_tx_by_hash")
-    files.sort()
+    # files = os.listdir("./evitar_tx_by_hash")
+    # files.sort()
     # print(files)
 
-    for file in files:
-        fileName = "./evitar_tx_by_hash/"+file
-        rTx.replayEvitar(w3,fileName,thresh,wnd)
-        rTx.backup(file)
+    # for file in files:
+    fileName = "./evitar_tx_by_hash/"+file
+    rTx.replayEvitar(w3, fileName, thresh, wnd)
 
-    # fileList = os.listdir("./evitar_tx_by_hash")
-    # fileList.sort()
-    file =  "./evitar_tx_by_hash/0xe.csv"
-          
-    rTx.replayEvitar(w3,file,thresh,wnd)
-
-    txF.writeTx(w3,'./result/result_evitar.csv',28)
-
-
-    
+    print(fileName)
+    resFile = './result/result_{}_{}_{}'.format(thresh, str(wnd), file)
+    print(resFile)
+    txF.writeTx(w3, './result/result_evitar.csv', 28)
