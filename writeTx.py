5912c622964d4671d291edd2262dc954cadd4c40
EarthSiwapol <siwapol@blockfint.com>
2020-10-11 21:07:34 +0700

    split tx

--- writeTx.py
+++ writeTx.py
@@ -19,15 +19,5 @@ if __name__ == "__main__":
     w3_2 = Web3(HTTPProvider('http://localhost:8544'))
     csv.field_size_limit(100000000)
 
-    file = sys.argv[1]
-
-    # files = os.listdir("./evitar_tx_by_hash")
-    # files.sort()
-    # print(files)
-
-    # for file in files:
-    fileName = "./evitar_tx_by_hash/"+file
-    rTx.replayBaseLineAndMaxGas(w3, w3_2, fileName)
-
-    # txF.writeTx(w3, './result/result_baseline_{}.csv'.format(file), 28)
-    # txF.writeTx(w3, './result/result_maxgas_{}.csv'.format(file), 28)
+    txF.writeTx(w3, './result/result_baseline_{}.csv'.format(file), 28)
+    txF.writeTx(w3, './result/result_maxgas_{}.csv'.format(file), 28)
