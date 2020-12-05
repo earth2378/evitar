e2c892f0fabf5638a6b816a95d53d8e57258b508
EarthSiwapol <siwapol@blockfint.com>
2020-12-05 13:59:21 +0700

    update var

--- bm_main.py
+++ bm_main.py
@@ -16,16 +16,12 @@ import src.txFunction as txF
 
 if __name__ == "__main__":
     w3 = Web3(HTTPProvider('http://localhost:8545'))
-    w3_2 = Web3(HTTPProvider('http://localhost:8544'))
+    # w3_2 = Web3(HTTPProvider('http://localhost:8544'))
     csv.field_size_limit(100000000)
 
     mode = sys.argv[1]
-    # files = os.listdir("./evitar_tx_by_hash")
-    # files.sort()
-    # print(files)
 
-    # low_mem = ['0x0.csv','0x3.csv','0x4.csv','0x5.csv','0x6.csv','0x7.csv','0x9.csv','0xa.csv','0xb.csv','0xc.csv','0xd.csv','0xe.csv','0xf.csv']
-    low_mem = ['0xe.csv']
+    low_mem = ['0x0.csv','0x3.csv','0x4.csv','0x5.csv','0x6.csv','0x7.csv','0x9.csv','0xa.csv','0xb.csv','0xc.csv','0xd.csv','0xe.csv','0xf.csv']
     high_mem = ['0x1.csv','0x2.csv','0x8.csv']
 
     if mode == 'low_mem':
@@ -41,5 +37,3 @@ if __name__ == "__main__":
     else:
         print('low_mem or high_mem only')
 
-    # txF.writeTx(w3, './result/result_baseline_{}.csv'.format(file), 28)
-    # txF.writeTx(w3, './result/result_maxgas_{}.csv'.format(file), 28)
