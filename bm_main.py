5212352c9e836795f3591102fa202eec112e3682
EarthSiwapol <siwapol@blockfint.com>
2020-12-06 20:40:19 +0700

    update cli

--- bm_main.py
+++ bm_main.py
@@ -19,21 +19,26 @@ if __name__ == "__main__":
     w3_2 = Web3(HTTPProvider('http://localhost:8544'))
     csv.field_size_limit(100000000)
 
-    mode = sys.argv[1]
+    file = sys.argv[1]
 
-    low_mem = ['0x0.csv','0x3.csv','0x4.csv','0x5.csv','0x6.csv','0x7.csv','0x9.csv','0xa.csv','0xb.csv','0xc.csv','0xd.csv','0xe.csv','0xf.csv']
-    high_mem = ['0x1.csv','0x2.csv','0x8.csv']
+    print(file)
+    fileName = "./evitar_tx_by_hash/"+file
 
-    if mode == 'low_mem':
-        for file in low_mem:
-            print(file)
-            fileName = "./evitar_tx_by_hash/"+file
-            rTx.replayBaseLineAndMaxGas(w3, w3_2, fileName)
-    elif mode == 'high_mem':
-        for file in high_mem:
-            print(file)
-            fileName = "./evitar_tx_by_hash/"+file
-            rTx.replayBaseLineAndMaxGas(w3, w3_2, fileName)
-    else:
-        print('low_mem or high_mem only')
+    rTx.replayBaseLineAndMaxGas(w3, w3_2, fileName)
+
+    # low_mem = ['0x0.csv','0x3.csv','0x4.csv','0x5.csv','0x6.csv','0x7.csv','0x9.csv','0xa.csv','0xb.csv','0xc.csv','0xd.csv','0xe.csv','0xf.csv']
+    # high_mem = ['0x1.csv','0x2.csv','0x8.csv']
+
+    # if mode == 'low_mem':
+    #     for file in low_mem:
+    #         print(file)
+    #         fileName = "./evitar_tx_by_hash/"+file
+    #         rTx.replayBaseLineAndMaxGas(w3, w3_2, fileName)
+    # elif mode == 'high_mem':
+    #     for file in high_mem:
+    #         print(file)
+    #         fileName = "./evitar_tx_by_hash/"+file
+    #         rTx.replayBaseLineAndMaxGas(w3, w3_2, fileName)
+    # else:
+    #     print('low_mem or high_mem only')
 
