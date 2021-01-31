ee39ee9425318cc5e66ceedee20915a1c3b4a6f0
siwapol <siwapol@blockfint.com>
2021-01-31 18:16:36 +0700

    update

--- countRow.py
+++ countRow.py
@@ -1,10 +1,14 @@
-import csv
+import csv, os, glob, sys
 
+path = sys.argv[1]
 csv.field_size_limit(100000000)
-csv_file = open('../database/baselineTxs/baseline_txs.csv','r+')
-csv_reader = csv.reader(csv_file, delimiter=',')
-
-row_count = sum(1 for row in csv_reader) 
-print(row_count)
-
-csv_file.close()
+# "../database/maxgasTxs_tmp"
+os.chdir(path)
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+for i in all_filenames:
+    csv_file = open(i,'r+')
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    row_count = sum(1 for row in csv_reader) 
+    print(i, row_count)
+    csv_file.close()
